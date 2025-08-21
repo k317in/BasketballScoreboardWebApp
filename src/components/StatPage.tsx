7c27fd1000308bee0d19e38c7944454aab8fb118
k317in <k317in@gmail.com>
2025-08-21 13:56:56 +0100

    Updated StatPage.tsx

--- src/components/StatPage.tsx
+++ src/components/StatPage.tsx
@@ -50,3 +50,6 @@ const StatPage: React.FC<StatPageProps> = ({ onBack, onLogin }) => {
   // Game clock timer - runs locally for real-time updates
   useEffect(() => {
     let interval: NodeJS.Timeout;
+  }
+  )
+}
